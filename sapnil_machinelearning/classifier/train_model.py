4721650ebfea7222f325f580d2eb43c4b7359e5f
rmisys123 <sapnil123@yahoo.com>
2020-09-01 02:33:15 +0600

    complete multi model detection

--- sapnil_machinelearning/classifier/train_model.py
+++ sapnil_machinelearning/classifier/train_model.py
@@ -8,7 +8,6 @@ from feature_eng.word_tf_idf import word_tf_idf
 from classifier.Classifier import train_model
 from dataset_pre.dataset_load import load_cvs_dataset
 import pickle
-from classifier.multinomial_nativebayes import live_multi_nativebayes_verna_predict
 #from classifier.multinomial_nativebayes import accuracy_score
 from sklearn.metrics import accuracy_score
 from builtins import str
@@ -62,87 +61,60 @@ def train_model_write(input_dataset, vocabulary_path, payload_col_name, payload_
 
 def live_verna_single_detection(model_path,web_param):
     dirs = os.listdir( model_path )
-    allmodel_result=[]
+    #allmodel_result=[]
     count_limit=len(dirs)-2
     count=0;
+    is_anom=False
     for pkl_file in dirs:
         file_num = int(re.search(r'\d+', pkl_file).group())
         classifier = pickle.load(open(str(model_path)+"text_classifier-"+str(file_num)+".pickle", mode='rb'))
         tfidf = pickle.load(open(str(model_path)+"tfidf-"+str(file_num)+".pickle", mode='rb'))
         imput_param= [''+str(web_param)]
         classifier_result = classifier.predict(tfidf.transform(imput_param))
-        allmodel_result.append(classifier_result[0])
+        if(classifier_result[0]=='anom'):
+            is_anom=True
+            break;
+        #allmodel_result.append(classifier_result[0])
         count=count+1
         if(count>=count_limit):
             break;
         
     
-    return allmodel_result
+    return is_anom
 
 
-def live_verna_detection(vocabulary_path, input_web_param_path,payload_col_name):
+def live_verna_detection(model_path, input_web_param_path,payload_col_name):
     
-
-    verify_result = []
+    
+    verify_result ='norm'   
     trainDF = load_cvs_dataset(input_web_param_path)
-    dirs = os.listdir( vocabulary_path )
-    file_no=len(dirs)
-   
     txt_text = trainDF[payload_col_name]
-    with open(str(vocabulary_path)+'tfidf.pickle', 'rb') as vocabulary_file:  
-        count_vect= pickle.load(vocabulary_file)
-    with open(str(vocabulary_path)+'text_classifier.pickle', 'rb') as training_model:  
-        model = pickle.load(training_model)
-       
     for web_param in txt_text:
-        X= [''+str(web_param)]
-        #print(model.predict(count_vect.transform(X)))
+        result=live_verna_single_detection(model_path,web_param)
+        if(result):
+            verify_result='anom'
+            break
         
-        subj = model.predict(count_vect.transform(X))
-       
-        verify_result.append(subj[0])
         #verify_result.append(model.predict(count_vect.transform(web_param)))    
-   
-    
     return verify_result
 
      
 
 
-def bulk_live_detection(train_model1, web_param):
-    
-    doc_class_label = live_multi_nativebayes_verna_predict(train_model1, web_param)     
-    # print("this class level is ",doc_class_label)
-    return doc_class_label
+
 
 
 def bulk_live_verna_detection(input_dataset, context_path, payload, label):
     bulk_verna_detect_result = []
     
-    with open(str(context_path)+'tfidf.pickl', 'rb') as vocabulary_file:  
-       count_vect= pickle.load(vocabulary_file)
-    with open(str(context_path)+'text_classifier.pickle', 'rb') as training_model:  
-        model = pickle.load(training_model)
-    
-   
     trainDF = load_cvs_dataset(input_dataset)
     #txt_label = trainDF[label]
     txt_text = trainDF[payload]
-    
-    
     for doc in txt_text:
-        X= [''+str(doc)]
-        doc_class_label = model.predict(count_vect.transform(X))
-        #bulk_verna_detect_result.append("" + str(doc) + "," + doc_class_label + "")
-        bulk_verna_detect_result.append(doc_class_label[0])
-        #bulk_verna_detect_result.append(doc)
-        #print("this doc_class_label is "+str(len(bulk_verna_detect_result)))
-    
+        result=live_verna_single_detection(context_path,doc)
+        bulk_verna_detect_result.append(result)
     return bulk_verna_detect_result
 
 
-def test_java_python_function(web_param):
-    print("the web param1556 :" + str(web_param))
-        
-    return ""
+
 
